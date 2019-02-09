0c0b1a04367a7779d76ac71fcb31032092c692af
Wenhan Jin <tigerjin4@berkeley.edu>
2019-02-08 22:00:15 -0800

    ll

--- tokenizer.c
+++ tokenizer.c
@@ -450,6 +450,7 @@ size_t SelectToken(char* buffer,
                  buffer[size_read])) {  // positive integers and some errors
     size_t int_len = 1;
     int search = 1;
+    printf("%s\n", buffer);
     printf("%d\n", buffer[size_read]);
     while (size_read + int_len < size && search) {
       if (is_digit(buffer[size_read + int_len])) {
