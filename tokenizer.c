4be87bf670088936337bd8da0bd641bdeb9f91f9
Wenhan Jin <tigerjin4@berkeley.edu>
2019-02-08 22:57:15 -0800

    ll

--- tokenizer.c
+++ tokenizer.c
@@ -399,7 +399,13 @@ size_t SelectToken(char* buffer,
                 t->data.character = replace_escape_in_character(buffer + size_read + 1);
                 size_read += 4;
             } else {
-              return size_read;
+              int total =
+                  generate_character_error(&t, buffer, size_read, size, *linenum, filename);
+              if (total == 0) {
+                  return size_read;
+              } else {
+                  size_read += total;
+              }
             }
      } else {
           int total =
